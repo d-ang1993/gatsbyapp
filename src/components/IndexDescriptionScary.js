9e16b93d3b9af4d4e5dcfe8955638debc9ba9da3
Daniel Ang <dan.justin.ang@gmail.com>
2019-03-13 00:11:42 -0400

    fixed grammatical errors

--- src/components/IndexDescriptionScary.js
+++ src/components/IndexDescriptionScary.js
@@ -11,8 +11,8 @@ class IndexDescriptionScary extends React.Component {
           Just a random fact about me but I really enjoy <span>scary</span>{' '}
           movies and stories despite how scared I am of them. The way people{' '}
           <span>interpret</span> old lores and fables and create something new
-          by <span>fusing</span> their <span>imagination</span> into it is
-          something I truly <span>appreciate</span>. That being said, I’m
+          by fusing their <span>imagination</span> into it is
+          something I truly appreciate. That being said, I’m
           currently working on making my next website a <span>ghost story</span>
           !{' '}
         </p>
